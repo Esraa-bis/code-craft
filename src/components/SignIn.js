df680725714339dc5c7d71933cd228a1a407d98d
Esraa Ali <esraaali.bis@gmail.com>
2024-05-07 00:08:27 +0300

    Implement sign in.

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,9 +1,54 @@
-import { Link } from "react-router-dom";
-import styles from "../assets/css/signForms.module.css";
+import { faEye, faEyeSlash } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
+import { Link, Navigate } from "react-router-dom";
 
-function SignIn() {
+import styles from "../assets/css/signForms.module.css";
+import { signIn } from "../services/auth";
+import { SessionTokenStorage } from "../services/local-storage";
+import { sweetAlert } from "../services/sweetalert";
+
+function SignIn({ signedIn, setSignedIn }) {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({});
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    if (!token) return;
+    SessionTokenStorage.saveToken(token);
+  }, [token]);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setLoading(true);
+    signIn(formData)
+      .then((response) => {
+        if (response.success) {
+          setSignedIn(true);
+          setToken(response.data.token);
+        } else {
+          sweetAlert({
+            title: response.message,
+            icon: "error",
+          });
+        }
+      })
+      .catch((error) => {
+        sweetAlert({ title: error.message, icon: "error" });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+
+  function updateFormData(event, fieldname) {
+    setFormData({ ...formData, [fieldname]: event.target.value });
+  }
   return (
     <section className={`${styles.signFormSection}`}>
+      {signedIn && <Navigate to="/" replace={true} />}
+
       <div className={`${styles.container}`}>
         <h2>Sign in</h2>
         {/* sign in with google btn */}
@@ -14,9 +59,8 @@ function SignIn() {
         <p></p>
         <form
           id="signInForm"
-          method="post"
-          api="auth/logIn"
           className={`${styles.signform}`}
+          onSubmit={handleSubmit}
         >
           <div className={`${styles.formGroup}`}>
             <label htmlFor="email">Email:</label>
@@ -25,20 +69,36 @@ function SignIn() {
               id="email"
               name="email"
               className={`${styles.input}`}
+              value={formData.email}
+              onChange={(event) => updateFormData(event, "email")}
               required
             />
           </div>
           <div className={`${styles.formGroup}`}>
             <label htmlFor="password">Password:</label>
             <input
-              type="password"
+              type={passwordVisible ? "text" : "password"}
               id="password"
               name="password"
               className={`${styles.input}`}
+              value={formData.password}
+              onChange={(event) => updateFormData(event, "password")}
               required
             />
+            <button
+              type="button"
+              onClick={() => setPasswordVisible(!passwordVisible)}
+            >
+              {passwordVisible ? (
+                <FontAwesomeIcon icon={faEye} />
+              ) : (
+                <FontAwesomeIcon icon={faEyeSlash} />
+              )}
+            </button>
           </div>
-          <button type="submit">Sign In</button>
+          <button type="submit" disabled={loading}>
+            Sign In
+          </button>
           <div className={`${styles.otherLinks}`}>
             <p>
               <Link to="/ForgotPassword" className={`${styles.link}`}>
@@ -48,7 +108,6 @@ function SignIn() {
             <p>
               If you don't have an account please
               <Link to="/SignUp" className={`${styles.link}`}>
-                {" "}
                 Sign-Up.
               </Link>
             </p>
