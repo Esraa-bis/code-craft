cc00797150e43c9893f30162e83bc61bb3e33b1a
Esraa Ali <esraaali.bis@gmail.com>
2024-05-23 04:52:44 +0300

    Upload courses for instructor

---
+++ src/pages/TeachOnCodeCraft.js
@@ -0,0 +1,169 @@
+import React, { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import styles from "../assets/css/TeachOnCodeCraft.module.css";
+import { getAllCategories } from "../services/admin";
+import { uploadCourseInfo } from "../services/course";
+import { sweetAlert } from "../services/sweetalert";
+
+const TeachOnCodeCraft = () => {
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({});
+  const [submited, setSubmited] = useState({});
+
+  useEffect(() => {
+    setLoading(true);
+    getAllCategories()
+      .then((response) => {
+        if (response.success) {
+          setCategories(response.categories);
+        } else {
+          setError("Failed to fetch Categories");
+        }
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setLoading(true);
+    uploadCourseInfo(formData)
+      .then((response) => {
+        if (response.success) {
+          setSubmited(true);
+          sweetAlert({
+            title: response.message,
+            icon: response.success ? "success" : "error",
+          });
+        }
+      })
+      .catch((error) => {
+        sweetAlert({ title: error.message, icon: "error" });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+
+  function updateFormData(event, fieldname) {
+    if (fieldname === "courseImage") {
+      setFormData({ ...formData, [fieldname]: event.target.files[0] });
+    } else {
+      setFormData({ ...formData, [fieldname]: event.target.value });
+    }
+  }
+
+  return (
+    <div className={styles.courseForm}>
+      {submited && <Navigate to="/UploadCourse" replace={true} />}
+
+      <h1>New / Edit Course</h1>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.formGroup}>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            name="title"
+            placeholder="Course title..."
+            id="title"
+            value={formData.name || ""}
+            onChange={(event) => updateFormData(event, "name")}
+            required
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="price">Price:</label>
+          <input
+            type="number"
+            min="0"
+            name="price"
+            placeholder="Course price..."
+            id="price"
+            value={formData.basePrice || ""}
+            onChange={(event) => updateFormData(event, "basePrice")}
+            required
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="category">Category:</label>
+          <select
+            id="category"
+            name="category"
+            required
+            value={formData.categoryId || ""}
+            onChange={(event) => updateFormData(event, "categoryId")}
+          >
+            <option value="" disabled selected>
+              Choose Category
+            </option>
+            {categories.map((category, index) => (
+              <option key={index} value={category._id}>
+                {category.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="level">Level:</label>
+          <select
+            id="level"
+            name="level"
+            required
+            value={formData.level || ""}
+            onChange={(event) => updateFormData(event, "level")}
+          >
+            <option value="" disabled selected>
+              Choose level
+            </option>
+            <option value="All">All</option>
+            <option value="Beginner">Beginner</option>
+            <option value="Intermediate">Intermediate</option>
+            <option value="Expert">Expert</option>
+          </select>
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="description">Description:</label>
+          <textarea
+            name="description"
+            placeholder="Course description..."
+            id="description"
+            value={formData.desc || ""}
+            onChange={(event) => updateFormData(event, "desc")}
+            required
+          ></textarea>
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="prerequisites">Prerequisites:</label>
+          <textarea
+            name="prerequisites"
+            placeholder="Course prerequisites..."
+            id="prerequisites"
+            value={formData.prerequisites || ""}
+            onChange={(event) => updateFormData(event, "prerequisites")}
+            required
+          ></textarea>
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="preview-image">Preview image:</label>
+          <input
+            type="file"
+            name="previewImage"
+            accept="image/*"
+            onChange={(event) => updateFormData(event, "courseImage")}
+            required
+          />
+        </div>
+        <button type="submit" className={styles.save} disabled={loading}>
+          Save & Continue
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default TeachOnCodeCraft;
--- src/pages/UploadCourse.js
+++ src/pages/UploadCourse.js
@@ -1,129 +1,178 @@
-import React, { useState } from "react";
-import "../assets/css/create-course.css"
-import { Link } from "react-router-dom";
-function UploadCourse () {
-  const [courseData, setCourseData] = useState({
-    title: "",
-    price: "",
-    category: "web",
-    level: "all",
-    description: "",
-    prerequisites: "",
-    previewImage: null,
-    coverPhoto: null,
-  });
+import React, { useState, useRef, useEffect } from "react";
+import styles from "../assets/css/UploadCourse.module.css";
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setCourseData({ ...courseData, [name]: value });
+function UploadCourse  ()  {
+  const [files, setFiles] = useState([]);
+  const [filesToDo, setFilesToDo] = useState(0);
+  const [filesDone, setFilesDone] = useState(0);
+  const dropAreaRef = useRef(null);
+  const progressBarRef = useRef(null);
+
+  useEffect(() => {
+    const dropArea = dropAreaRef.current;
+
+    const preventDefaults = (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+    };
+
+    ["dragenter", "dragover", "dragleave", "drop"].forEach((eventName) => {
+      dropArea.addEventListener(eventName, preventDefaults, false);
+    });
+
+    const highlight = () => dropArea.classList.add(styles.highlight);
+    const unhighlight = () => dropArea.classList.remove(styles.highlight);
+
+    ["dragenter", "dragover"].forEach((eventName) => {
+      dropArea.addEventListener(eventName, highlight, false);
+    });
+
+    ["dragleave", "drop"].forEach((eventName) => {
+      dropArea.addEventListener(eventName, unhighlight, false);
+    });
+
+    const handleDrop = (e) => {
+      const dt = e.dataTransfer;
+      const files = dt.files;
+      handleFiles(files);
+    };
+
+    dropArea.addEventListener("drop", handleDrop, false);
+
+    return () => {
+      ["dragenter", "dragover", "dragleave", "drop"].forEach((eventName) => {
+        dropArea.removeEventListener(eventName, preventDefaults, false);
+      });
+
+      ["dragenter", "dragover"].forEach((eventName) => {
+        dropArea.removeEventListener(eventName, highlight, false);
+      });
+
+      ["dragleave", "drop"].forEach((eventName) => {
+        dropArea.removeEventListener(eventName, unhighlight, false);
+      });
+
+      dropArea.removeEventListener("drop", handleDrop, false);
+    };
+  }, []);
+
+  const handleFiles = (fileList) => {
+    const newFiles = [...fileList];
+    setFiles(newFiles);
+    initializeProgress(newFiles.length);
+    newFiles.forEach(previewFile);
   };
 
-  const handleFileChange = (e) => {
-    const { name, files } = e.target;
-    setCourseData({ ...courseData, [name]: files[0] });
+  const initializeProgress = (numFiles) => {
+    setFilesDone(0);
+    setFilesToDo(numFiles);
+    progressBarRef.current.value = 0;
   };
 
-  const handleSubmit = () => {
-    // Handle form submission here
-    console.log(courseData);
+  const progressDone = () => {
+    setFilesDone((prev) => {
+      const updatedFilesDone = prev + 1;
+      progressBarRef.current.value = (updatedFilesDone / filesToDo) * 100;
+      return updatedFilesDone;
+    });
+  };
+
+  const formatDuration = (durationInSeconds) => {
+    const hours = Math.floor(durationInSeconds / 3600);
+    const minutes = Math.floor((durationInSeconds % 3600) / 60);
+    const seconds = Math.floor(durationInSeconds % 60);
+
+    const formattedHours = hours > 0 ? `${hours}h ` : "";
+    const formattedMinutes = minutes > 0 ? `${minutes}m ` : "";
+    const formattedSeconds = seconds > 0 ? `${seconds}s` : "";
+
+    return formattedHours + formattedMinutes + formattedSeconds;
+  };
+
+  const previewFile = (file, index) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onloadend = () => {
+      const group = document.createElement("div");
+      const label = document.createElement("label");
+      const input = document.createElement("input");
+      const video = document.createElement("video");
+      const duration = document.createElement("h5");
+      const del = document.createElement("button");
+      const actions = document.createElement("div");
+
+      group.className = styles.videoGroup;
+      actions.className = styles.actions;
+      duration.className = styles.duration;
+      del.className = styles.removeVideo;
+
+      del.innerText = "X";
+      del.onclick = () => {
+        const confirmed = window.confirm(
+          "Are you sure you want to remove this video?"
+        );
+        if (confirmed) {
+          setFiles((prevFiles) => prevFiles.filter((_, i) => i !== index));
+          group.remove();
+        }
+      };
+
+      input.placeholder = "Video title...";
+      input.value = file.name.replace(/\.[A-Za-z0-9]{0,}$/, "");
+      input.id = `video${index}`;
+
+      label.setAttribute("for", `video${index}`);
+      video.src = reader.result;
+      video.preload = "metadata";
+      duration.innerText = "0s";
+
+      video.oncanplay = () => {
+        duration.innerText = formatDuration(video.duration || 0);
+      };
+
+      group.appendChild(label);
+      actions.append(duration);
+      actions.append(del);
+      label.appendChild(actions);
+      label.appendChild(video);
+      label.appendChild(input);
+      document.querySelector(`.${styles.videos}`).appendChild(group);
+    };
   };
 
   return (
-    <div className="courseForm">
-      <h1>New / Edit Course</h1>
-      <form>
-        <div className="formGroup">
-          <label htmlFor="title">Title:</label>
-          <input
-            type="text"
-            name="title"
-            placeholder="Course title..."
-            id="title"
-            required
-            onChange={handleChange}
-          />
-        </div>
-        <div className="formGroup">
-          <label htmlFor="price">Price:</label>
-          <input
-            type="number"
-            min="0"
-            name="price"
-            placeholder="Course price..."
-            id="price"
-            required
-            onChange={handleChange}
-          />
-        </div>
-        <div className="formGroup">
-          <label htmlFor="category">Category:</label>
-          <select
-            id="category"
-            name="category"
-            required
-            onChange={handleChange}
-          >
-            <option value="web">Web Development</option>
-            <option value="mobile">Mobile Development</option>
-            <option value="game">Game Development</option>
-            <option value="testing">Software Testing</option>
-            <option value="engineering">Software Engineering</option>
-            <option value="database">Database Design & Development</option>
-            <option value="programming">Programming</option>
-          </select>
-        </div>
-        <div className="formGroup">
-          <label htmlFor="level">Level:</label>
-          <select id="level" name="level" required onChange={handleChange}>
-            <option value="all">All</option>
-            <option value="beginner">Beginner</option>
-            <option value="intermediate">Intermediate</option>
-            <option value="expert">Expert</option>
-          </select>
-        </div>
-        <div className="formGroup">
-          <label htmlFor="description">Description:</label>
-          <textarea
-            name="description"
-            placeholder="Course description..."
-            id="description"
-            required
-            onChange={handleChange}
-          ></textarea>
-        </div>
-        <div className="formGroup">
-          <label htmlFor="prerequisites">Prerequisites:</label>
-          <textarea
-            name="prerequisites"
-            placeholder="Course prerequisites..."
-            id="prerequisites"
-            required
-            onChange={handleChange}
-          ></textarea>
-        </div>
-        <div className="formGroup">
-          <label htmlFor="preview-image">Preview image:</label>
+    <div className={styles.courseForm}>
+      <h1>My Course Title Content</h1>
+      <div className={styles.dropArea} ref={dropAreaRef}>
+        <form className={styles.form}>
+          <p>
+            Upload multiple files with the file dialog or by dragging and
+            dropping images onto the dashed region
+          </p>
           <input
             type="file"
-            name="previewImage"
-            accept="image/*"
-            onChange={handleFileChange}
+            className={styles.fileInput}
+            id="fileInput"
+            multiple
+            accept="video/*"
+            onChange={(e) => handleFiles(e.target.files)}
           />
-        </div>
-        <div className="formGroup">
-          <label htmlFor="cover-photo">Cover photo:</label>
-          <input
-            type="file"
-            name="coverPhoto"
-            accept="image/*"
-            onChange={handleFileChange}
-          />
-        </div>
-        <button type="button" className="save" onClick={handleSubmit}>
-          <Link to="/UploadVideos">Save & Continue</Link>
-        </button>
-      </form>
-      {/* Add more comments here if needed */}
+          <label className={styles.button} htmlFor="fileInput">
+            Select videos
+          </label>
+        </form>
+        <progress
+          id="progressBar"
+          max="100"
+          value="0"
+          ref={progressBarRef}
+        ></progress>
+      </div>
+      <div className={styles.videos}></div>
+      <button type="button" className={styles.save}>
+        Upload & Save
+      </button>
     </div>
   );
 };
