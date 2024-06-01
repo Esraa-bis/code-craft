c3efe9ced74e04d10ba6e38a6d00e08c4599c289
Esraa Ali <esraaali.bis@gmail.com>
2024-06-02 01:06:17 +0300

    Get categories if courses filters.

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import styles from "../assets/css/courses.module.css";
 import CourseCard from "../components/CourseCard";
+import { getAllCategories } from "../services/admin";
 import { getCoursesFilters } from "../services/course";
 
 function Courses() {
@@ -16,11 +17,10 @@ function Courses() {
     const filters = {
       isApproved: true,
     };
-
     getCoursesFilters(filters)
       .then((response) => {
         if (response.success) {
-          setCourses(response.courses);
+          setCourses(response.coursesWithEnrollment);
         } else {
           setError("Failed to fetch courses");
         }
@@ -56,6 +56,34 @@ function Courses() {
 export default Courses;
 
 function Filters() {
+  const [formData, setFormData] = useState({});
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+  let loading = false;
+  const setLoading = (value) => {
+    loading = value;
+  };
+
+  useEffect(() => {
+    if (loading || loaded) return;
+    setLoading(true);
+    getAllCategories()
+      .then((response) => {
+        if (response.success) {
+          setCategories(response.categories);
+          setLoaded(true);
+        } else {
+          setError("Failed to fetch Categories");
+        }
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div className={styles.filters}>
       <h4>
@@ -78,34 +106,12 @@ function Filters() {
 
       <div className={`${styles.checkboxList} ${styles.categories}`}>
         <h6 className={`${styles.FilterName}`}>Categories</h6>
-        <label>
-          <input type="checkbox" name="WebDevelopment" />
-          Web Development
-        </label>
-        <label>
-          <input type="checkbox" name="MobileDevelopment" />
-          Mobile Development
-        </label>
-        <label>
-          <input type="checkbox" name="ProgrammingLanguages" />
-          Programming Languages
-        </label>
-        <label>
-          <input type="checkbox" name="GameDevelopment" />
-          Game Development
-        </label>
-        <label>
-          <input type="checkbox" name="DatabaseDesignanddevelopment" />
-          Database Design and development
-        </label>
-        <label>
-          <input type="checkbox" name="SoftwareTesting" />
-          Software Testing
-        </label>
-        <label>
-          <input type="checkbox" name="SoftwareEngineering" />
-          Software Engineering
-        </label>
+        {categories?.map((category, index) => (
+          <label key={index} value={category._id}>
+            <input type="checkbox" name="WebDevelopment" />
+            {category.name}
+          </label>
+        ))}
       </div>
       <div className={`${styles.checkboxList} ${styles.categories}`}>
         <div className={`${styles.checkboxList} ${styles.categories}`}>
@@ -151,7 +157,7 @@ function Filters() {
           Intermediate
         </label>
         <label>
-          <input type="checkbox" name="Expert" />
+          <input type="checkbox" name="Advanced" />
           Expert
         </label>
       </div>
@@ -346,7 +352,6 @@ function Filters() {
                 d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
               />
             </svg>
-
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -420,7 +425,6 @@ function Filters() {
                 d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
               />
             </svg>
-
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -496,7 +500,6 @@ function Filters() {
                 d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
               />
             </svg>
-
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
