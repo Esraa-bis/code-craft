0744102a14d16108fa8ea648527ba12cea71674c
Esraa Ali <esraaali.bis@gmail.com>
2024-06-05 03:54:44 +0300

    Add function to check if user logged in or not and use it.

--- src/services/generalFunctions.js
+++ src/services/generalFunctions.js
@@ -1,3 +1,5 @@
+import { sweetAlert } from "./sweetalert";
+
 export const convertMinutes = (duration) => {
   const hours = Math.floor(duration / 60);
   const minutes = Math.round(duration % 60);
@@ -7,4 +9,13 @@ export const convertMinutes = (duration) => {
   } else {
     return `${minutes} m`;
   }
+};
+export const checkLogin = (islogedIn) => {
+  if (!islogedIn) {
+    sweetAlert({
+      title: "Please login first!",
+    });
+    return false;
+  }
+  return true;
 };
