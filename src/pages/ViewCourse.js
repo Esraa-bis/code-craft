e165ac70df9dc9081b7d2ae1b0af1a76acff8924
Esraa Ali <esraaali.bis@gmail.com>
2024-06-05 17:10:26 +0300

    Remove enrollment btn if user  allredt enrolled.

--- src/pages/ViewCourse.js
+++ src/pages/ViewCourse.js
@@ -18,6 +18,8 @@ function ViewCourse() {
   }
 
   const [course, setCourse] = useState(null);
+  const [isEnrolled, setIsEnrolled] = useState(null);
+
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const query = useQuery();
@@ -35,6 +37,7 @@ function ViewCourse() {
       .then((response) => {
         if (response.success) {
           setCourse(response.course);
+          setIsEnrolled(response.isEnrolled);
         } else {
           setError("Failed to fetch course preview");
         }
@@ -176,38 +179,47 @@ function ViewCourse() {
               />
               <FontAwesomeIcon icon={faPlay} className={` icon `} />
             </Link>
-            <h6 className={styles.price}>Price: {course.appliedPrice} EGP</h6>
-            <div className={styles.allBtns}>
-              <div className={styles.cartAndWishList}>
-                <div className={styles.innerDiv}>
-                  <button
-                    onClick={() => handleAddToCart(course?._id)}
-                    className={styles.AddToCartIcon}
-                  >
-                    Add to Cart
-                  </button>
-                  <button className={styles.wishListIcon}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6"
+            {isEnrolled ? null : (
+              <h6 className={styles.price}>Price: {course.appliedPrice} EGP</h6>
+            )}
+            {isEnrolled ? null : (
+              <div className={styles.allBtns}>
+                <div className={styles.cartAndWishList}>
+                  <div className={styles.innerDiv}>
+                    <button
+                      onClick={() => handleAddToCart(course?._id)}
+                      className={styles.AddToCartIcon}
                     >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
-                      />
-                    </svg>
-                  </button>
+                      Add to Cart
+                    </button>
+                    <button className={styles.wishListIcon}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                        />
+                      </svg>
+                    </button>
+                  </div>
                 </div>
+                <button className={styles.buyNow}>
+                  <Link
+                    to={`/Checkout?courseId=${course?._id}`}
+                    className={buyNowLink}
+                  >
+                    Buy Now
+                  </Link>
+                </button>
               </div>
-              <button className={styles.buyNow}>
-                <Link to={`/Checkout?courseId=${course?._id}`}>Buy Now</Link>
-              </button>
-            </div>
+            )}
           </div>
         </section>
         <section>
