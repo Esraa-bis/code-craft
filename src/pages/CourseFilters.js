39459d757c05d5685fce04ebd420ae3e6be0ab11
Esraa Ali <esraaali.bis@gmail.com>
2024-06-22 17:29:50 +0300

    Remove unneeded comment.

--- src/pages/CourseFilters.js
+++ src/pages/CourseFilters.js
@@ -70,35 +70,10 @@ function CourseFilters({ setFilters, filtersVisible }) {
         setLoading(false);
       });
   }, []);
-  //
-  //
-  // const [filtersVisible, setFiltersVisible] = useState(false);
 
-  // const toggleFiltersVisibility = () => {
-  //   setFiltersVisible(!filtersVisible);
-  // };
   return (
     <>
       <div className={styles.filtersDiv}>
-        {/* <button className={styles.filtersBtn} onClick={toggleFiltersVisibility}>
-          <p>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className={`w-6 h-6 ${styles.filterIcon}`}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75"
-              />
-            </svg>
-            Filters
-          </p>
-        </button> */}
         <div
           className={`${styles.filters} ${
             filtersVisible ? styles.visible : ""
